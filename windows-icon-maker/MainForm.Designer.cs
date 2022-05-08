ca5ca6254f71979f483a4562779840796b412831
jean-knapp <email+github@jeankn.app>
2022-05-08 14:30:26 -0300

    Initial commit.

---
+++ windows-icon-maker/MainForm.Designer.cs
@@ -0,0 +1,241 @@
+﻿
+namespace windows_icon_maker
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openBaseImageButton = new windows_knapp_winforms.Controls.Button();
+            this.pictureBox256 = new System.Windows.Forms.PictureBox();
+            this.pictureBox48 = new System.Windows.Forms.PictureBox();
+            this.pictureBox32 = new System.Windows.Forms.PictureBox();
+            this.pictureBox16 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.exportButton = new windows_knapp_winforms.Controls.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox256)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox48)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox32)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 68);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(144, 146);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(20, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Original";
+            // 
+            // openBaseImageButton
+            // 
+            this.openBaseImageButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.openBaseImageButton.Caption = "Import";
+            this.openBaseImageButton.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.openBaseImageButton.Location = new System.Drawing.Point(92, 220);
+            this.openBaseImageButton.Name = "openBaseImageButton";
+            this.openBaseImageButton.Padding = new System.Windows.Forms.Padding(1);
+            this.openBaseImageButton.Size = new System.Drawing.Size(75, 23);
+            this.openBaseImageButton.TabIndex = 4;
+            this.openBaseImageButton.Click += new System.EventHandler(this.openBaseImageButton_Click);
+            // 
+            // pictureBox256
+            // 
+            this.pictureBox256.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox256.Location = new System.Drawing.Point(190, 68);
+            this.pictureBox256.Name = "pictureBox256";
+            this.pictureBox256.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox256.TabIndex = 5;
+            this.pictureBox256.TabStop = false;
+            // 
+            // pictureBox48
+            // 
+            this.pictureBox48.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox48.Location = new System.Drawing.Point(452, 68);
+            this.pictureBox48.Name = "pictureBox48";
+            this.pictureBox48.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox48.TabIndex = 6;
+            this.pictureBox48.TabStop = false;
+            // 
+            // pictureBox32
+            // 
+            this.pictureBox32.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox32.Location = new System.Drawing.Point(506, 68);
+            this.pictureBox32.Name = "pictureBox32";
+            this.pictureBox32.Size = new System.Drawing.Size(32, 32);
+            this.pictureBox32.TabIndex = 7;
+            this.pictureBox32.TabStop = false;
+            // 
+            // pictureBox16
+            // 
+            this.pictureBox16.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox16.Location = new System.Drawing.Point(544, 68);
+            this.pictureBox16.Name = "pictureBox16";
+            this.pictureBox16.Size = new System.Drawing.Size(16, 16);
+            this.pictureBox16.TabIndex = 8;
+            this.pictureBox16.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(187, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "256x256";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(449, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(36, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "48x48";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(541, 52);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(36, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "16x16";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(503, 52);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "32x32";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(187, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(37, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Result";
+            // 
+            // exportButton
+            // 
+            this.exportButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.exportButton.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.exportButton.Caption = "Export";
+            this.exportButton.HoverColor = System.Drawing.Color.FromArgb(((int)(((byte)(68)))), ((int)(((byte)(68)))), ((int)(((byte)(68)))));
+            this.exportButton.Location = new System.Drawing.Point(504, 342);
+            this.exportButton.Name = "exportButton";
+            this.exportButton.Padding = new System.Windows.Forms.Padding(1);
+            this.exportButton.Size = new System.Drawing.Size(75, 23);
+            this.exportButton.TabIndex = 14;
+            this.exportButton.Click += new System.EventHandler(this.exportButton_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 373);
+            this.Controls.Add(this.exportButton);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox16);
+            this.Controls.Add(this.pictureBox32);
+            this.Controls.Add(this.pictureBox48);
+            this.Controls.Add(this.pictureBox256);
+            this.Controls.Add(this.openBaseImageButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Location = new System.Drawing.Point(0, 0);
+            this.MaximumSize = new System.Drawing.Size(586, 373);
+            this.MinimumSize = new System.Drawing.Size(586, 373);
+            this.Name = "MainForm";
+            this.Text = "Icon Maker";
+            this.Controls.SetChildIndex(this.pictureBox1, 0);
+            this.Controls.SetChildIndex(this.label1, 0);
+            this.Controls.SetChildIndex(this.openBaseImageButton, 0);
+            this.Controls.SetChildIndex(this.pictureBox256, 0);
+            this.Controls.SetChildIndex(this.pictureBox48, 0);
+            this.Controls.SetChildIndex(this.pictureBox32, 0);
+            this.Controls.SetChildIndex(this.pictureBox16, 0);
+            this.Controls.SetChildIndex(this.label2, 0);
+            this.Controls.SetChildIndex(this.label3, 0);
+            this.Controls.SetChildIndex(this.label4, 0);
+            this.Controls.SetChildIndex(this.label5, 0);
+            this.Controls.SetChildIndex(this.label6, 0);
+            this.Controls.SetChildIndex(this.exportButton, 0);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox256)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox48)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox32)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox16)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private windows_knapp_winforms.Controls.Button openBaseImageButton;
+        private System.Windows.Forms.PictureBox pictureBox256;
+        private System.Windows.Forms.PictureBox pictureBox48;
+        private System.Windows.Forms.PictureBox pictureBox32;
+        private System.Windows.Forms.PictureBox pictureBox16;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private windows_knapp_winforms.Controls.Button exportButton;
+    }
+}
---
+++ windows-icon-maker/MainForm.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Windows.Forms;
+using windows_knapp_winforms.Forms;
+
+namespace windows_icon_maker
+{
+    public partial class MainForm : BaseForm
+    {
+        Bitmap OriginalBitmap = null;
+        string OriginalFilePath { get; set; } = "";
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void openBaseImageButton_Click(object sender, System.EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog()
+            {
+                Filter = "Portable Network Graphic (*.png)|*.png|JPEG (*.jpg;*.jpeg;*.jpe;*.jfif;*.jfi)|*.jpg;*.jpeg;*.jpe;*.jfif;*.jfi|Bitmap (*.bmp)|*.bmp|Graphics Interchange Format (*.gif)|*.gif|Icon (*.ico)|*.ico|Tagged Image File Format (*.tif)|*.tiff|Exchangeable image file format (*.exif)|*.exif|EMF (*.emf)|*.emf|WMF (*.wmf)|*.wmf"
+            };
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    OriginalBitmap = (Bitmap)Bitmap.FromFile(dialog.FileName);
+
+                    ReplacePictureboxImage(pictureBox1, OriginalBitmap);
+
+                    OriginalFilePath = dialog.FileName;
+
+                    ReplacePictureboxImage(pictureBox256, ResizeBitmap(OriginalBitmap, 256, 256));
+                    ReplacePictureboxImage(pictureBox48, ResizeBitmap(OriginalBitmap, 48, 48));
+                    ReplacePictureboxImage(pictureBox32, ResizeBitmap(OriginalBitmap, 32, 32));
+                    ReplacePictureboxImage(pictureBox16, ResizeBitmap(OriginalBitmap, 16, 16));
+                } catch (Exception)
+                {
+                    System.Diagnostics.Debugger.Break();
+                }
+            }
+        }
+
+        private void ReplacePictureboxImage(PictureBox pictureBox, Bitmap bitmap)
+        {
+            if (pictureBox.Image != null)
+                pictureBox.Image.Dispose();
+
+            pictureBox.Image = bitmap;
+        }
+
+        private Bitmap ResizeBitmap(Bitmap bitmap, int width, int height)
+        {
+            Bitmap bmp = new Bitmap(bitmap, new Size(width, height));
+            using (Graphics graphics = Graphics.FromImage(bmp)) {
+                graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                graphics.CompositingQuality = CompositingQuality.HighQuality;
+                graphics.SmoothingMode = SmoothingMode.AntiAlias;
+                graphics.DrawImage(bmp, width, height);
+            }
+            return bmp;
+        }
+
+        private void exportButton_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog dialog = new SaveFileDialog()
+            {
+                Filter = "Icon (*.ico)|*.ico"
+            };
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                using (var stream = new FileStream(dialog.FileName, FileMode.Create))
+                {
+                    IconFactory.SavePngsAsIcon(new[] { (Bitmap)pictureBox16.Image, (Bitmap)pictureBox32.Image, (Bitmap)pictureBox48.Image, (Bitmap)pictureBox256.Image }, stream);
+                }
+            }
+        }
+    }
+}
